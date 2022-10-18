cad817d3fe80959c0cebab6bb53fda1d1a45c714
chiranwettewa <chiranwettewa@gmail.com>
2022-10-18 18:06:45 +0530

    final push

--- src/main/java/com/dms/inventorymanagementservice/InventoryManagementServiceApplication.java
+++ src/main/java/com/dms/inventorymanagementservice/InventoryManagementServiceApplication.java
@@ -1,7 +1,11 @@
 package com.dms.inventorymanagementservice;
 
+import com.dms.inventorymanagementservice.model.Inventory;
+import com.dms.inventorymanagementservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryManagementServiceApplication {
@@ -10,4 +14,20 @@ public class InventoryManagementServiceApplication {
 		SpringApplication.run(InventoryManagementServiceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iphone_13_red");
+			inventory.setQuantity(100);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("iphone_13_red");
+			inventory1.setQuantity(0);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+
+		};
+	}
 }
